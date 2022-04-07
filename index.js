fc1171aaa785552e69c8034b3bb311076cc5d25c
NerovnuiMihail <nerovnuimihail@gmail.com>
2022-04-07 13:03:18 +0300

    init project

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+
+const app = express();
+const hbs = exphbs.create({extname: 'hbs'});
+app.engine('hbs', hbs.engine);
+app.set('view engine','hbs');
+app.set('views','views');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home'
+    })
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started an: http://localhost:${PORT}`));
