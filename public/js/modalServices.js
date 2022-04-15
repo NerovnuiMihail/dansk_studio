b6b4f4f49dc5b8ffa6375ad06906cb83b583a06c
NerovnuiMihail <nerovnuimihail@gmail.com>
2022-04-15 20:34:54 +0300

    update modalServices

--- public/js/modalServices.js
+++ public/js/modalServices.js
@@ -5,6 +5,9 @@ const tabContentItems = document.querySelectorAll('.tab-content__item');
 const openServicesBtn = document.querySelector('.utils-btn-course');
 const servicesContainer = document.querySelector('.services-utils');
 const servicesForm = document.forms.services_utils;
+const selectCourseName = servicesForm.elements.coursename;
+const selectCourseTime = servicesForm.elements.time;
+const tabNames = ['authors course','individual lesson','group lesson'];
 let tabIdx = 0;
 
 function clearTab() {
@@ -23,24 +26,91 @@ function showTabItem(idx) {
     tabContentItems[idx].style.display = '';
 }
 
-clearTab();
-hideTabItems();
-choiceTab(tabIdx);
-showTabItem(tabIdx);
-renderSelector(tabIdx);
-renderOptions(tabIdx);
+async function fetchSelectData(idx) {
+    const URL = 'http://localhost:3000/api';
+    const db = await fetch(URL);
+    const dataApi = await db.json();
+    return dataApi[idx];
+}
+
+async function renderSelector(idx) {
+    const dataApi = await fetchSelectData(idx);
+    const data = dataApi[tabNames[idx]];
+    selectCourseName.innerHTML = '';
+
+    data.forEach(item => {
+        const option = document.createElement('option');
+        option.textContent = item.title;
+        option.value = item.title;
+        selectCourseName.append(option);
+    });
+}
+
+async function renderOptions(idx) {
+    const fildset = document.querySelector('.services-utils__fildset');
+    const removeTime = document.querySelector('.remTime');
+
+    const dataApi = await fetchSelectData(idx);
+    const data = dataApi[tabNames[idx]];
+    const currentCourse = data.filter(item => item.title === selectCourseName.value)[0];
+    selectCourseTime.innerHTML = '';
+
+    if (idx === 0) {
+        removeTime.style.display = 'none';
+        selectCourseTime.style.display = 'none';
+        servicesForm.style.height = '701px'; 
+        fildset.style.marginTop = '70px';
+    } else {
+        removeTime.style.display = 'block';
+        selectCourseTime.style.display = 'block';
+        servicesForm.style.height = '748px'; 
+        fildset.style.marginTop = '';
+
+        currentCourse.time.forEach(item => {
+            const option = document.createElement('option');
+            option.textContent = item;
+            option.value = item;
+            selectCourseTime.append(option);
+        });
+    }
+}
+
+async function renderDescription(idx, selectValue) {
+    const dataApi = await fetchSelectData(idx);
+    const data = dataApi[tabNames[idx]];
+    const currentCourse = data.filter(item => item.title === selectCourseName.value)[0];
+    const costInd = currentCourse.time.findIndex(timeIdx => timeIdx === selectValue) !== -1 ? currentCourse.time.findIndex(timeIdx => timeIdx === selectValue) : 0;
+
+    const descriptionContainer = document.querySelector('.services-utils__descriptionContainer');
+    const time = document.createElement('p');
+    const cost = document.createElement('p');
+
+    descriptionContainer.textContent = '';
+    descriptionContainer.style.width = '823px';
+
+    if (idx === 0) {
+        time.textContent = `Продолжительность: ${currentCourse.time[0]}`;
+        cost.textContent = `Стоимость: ${currentCourse.cost[0]} крон.`;
+        descriptionContainer.append(time);
+        descriptionContainer.append(cost);
+    } else {
+        time.textContent = `Продолжительность: индивидуальное количество, уроки 60-70мин`;
+        cost.textContent = `Стоимость: ${currentCourse.cost[costInd]} крон за занятие.`;
+        descriptionContainer.append(time);
+        descriptionContainer.append(cost);
+    }
+}
 
 tabItems.forEach((item,idx) => {
     item.addEventListener('click', () => {
         tabIdx = idx;
         clearTab();
         hideTabItems();
-
         choiceTab(tabIdx);
         showTabItem(tabIdx);
-
         renderSelector(tabIdx);
         renderOptions(tabIdx);
+        renderDescription(tabIdx);
     });
 });
 
@@ -54,66 +124,19 @@ servicesContainer.addEventListener('click', (e) => {
     }
 });
 
+selectCourseTime.addEventListener('change', (e) => {
+    renderDescription(tabIdx, e.target.value);
+});
 
-//////
+selectCourseName.addEventListener('change', (e) => {
+    renderOptions(tabIdx);
+    renderDescription(tabIdx, e.target.value);
+});
 
-
-const selectCourseName = document.querySelector('#coursename');
-const selectCourseTime = document.querySelector('#time');
-const descriptionContainer = document.querySelector('.services-utils__descriptionContainer');
-const tabNames = ['authors course','individual lesson','group lesson'];
-
-async function fetchSelectData(idx) {
-    const URL = 'http://localhost:3000/api';
-    const db = await fetch(URL);
-    const dataApi = await db.json();
-    return dataApi[idx];
-}
-
-async function renderSelector(idx) {
-    const dataApi = await fetchSelectData(idx);
-    const data = dataApi[tabNames[idx]];
-    selectCourseName.innerHTML = '';
-
-    console.log(data);
-
-    data.forEach(item => {
-        const option = document.createElement('option');
-        option.textContent = item.title;
-        option.value = item.title;
-        selectCourseName.append(option);
-    });
-}
-
-async function renderOptions(idx) {
-    const dataApi = await fetchSelectData(idx);
-    const data = dataApi[tabNames[idx]];
-    const currentCourse = data.filter(item => item.title === selectCourseName.value)[0];
-    selectCourseTime.innerHTML = '';
-
-    currentCourse.time.forEach(item => {
-        const option = document.createElement('option');
-        option.textContent = item;
-        option.value = item;
-        selectCourseTime.append(option);
-
-
-        // if (idx !== 0) {
-        //     const option = document.createElement('option');
-        //     option.textContent = item;
-        //     option.value = item;
-        //     selectCourseTime.append(option);
-        // } else {
-        //     document.querySelector('.remTime').remove();
-        //     servicesForm.elements.time.remove();
-    
-        //     const p = document.createElement('p');
-        //     p.textContent = `Продолжительность: ${item}`;
-        //     servicesForm.elements[0].append(p);
-        // }
-    });
-}
-
-function renderDescription() {
-
-}
+clearTab();
+hideTabItems();
+choiceTab(tabIdx);
+showTabItem(tabIdx);
+renderSelector(tabIdx);
+renderOptions(tabIdx);
+renderDescription(tabIdx);
